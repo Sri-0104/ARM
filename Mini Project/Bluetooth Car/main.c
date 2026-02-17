cf6facef95ef0fe8a35d1bca187b256bab35db91
Srisuryaprakash <79654026+Sri-0104@users.noreply.github.com>
2026-02-17 22:30:17 +0530

    Update main.c

--- Mini Project/Bluetooth Car/main.c
+++ Mini Project/Bluetooth Car/main.c
@@ -1,1 +1,26 @@
+#include <LPC21xx.h>
+#include "lcd.h"
+#include "uart.h"
+#include "motor.h"
+
+int main()
+{
+		unsigned char rxbyte;
+		LCD_init();
+		LCD_cmd(0x82);
+		LCD_str("Bluetooth Car");
+		UART_config();
+		motor_config();
+		while(1)
+		{
+			rxbyte = UART_Rx();
+			switch (rxbyte)
+			{
+				case 'F':	;
+			}
+		}
+}
+
+
+
 

038f126f07397441251246c518802b81396fc3f0
Srisuryaprakash <79654026+Sri-0104@users.noreply.github.com>
2026-02-17 22:20:48 +0530

    Create motor.h

---
+++ Mini Project/Bluetooth Car/motor.h
@@ -0,0 +1,19 @@
+#include <LPC21xx.h>
+
+void UART_config(void);
+unsigned char UART_Rx(void);
+
+void UART_config(void)
+{
+	PINSEL0 = 0x05;
+	U0LCR = 0x83;
+	U0DLL = 97;
+	U0DLM = 0;
+	U0LCR = 0x03;
+}
+
+unsigned char UART_Rx(void)
+{
+	while((U0LSR&0)==0);
+	return U0RBR;
+}

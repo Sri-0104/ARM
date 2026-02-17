f46302aa94871a1628c454325a10368bc80db693
Srisuryaprakash <79654026+Sri-0104@users.noreply.github.com>
2026-02-17 22:29:55 +0530

    Update ldc.h

--- Mini Project/Bluetooth Car/ldc.h
+++ Mini Project/Bluetooth Car/ldc.h
@@ -1,1 +1,58 @@
+#include <LPC21xx.h>
+
+#define LCD_D 0xff
+#define RS 1<<8
+#define E  1<<9
+
+void LCD_init(void);
+void LCD_cmd(unsigned int);
+void delayms(unsigned int);
+void LCD_data(unsigned char);
+void LCD_str(unsigned char*);
+
+void delayms(unsigned int ms)
+{
+	T0PR = 15000-1;
+	T0TCR = 0x01;
+	while(T0TC<ms);
+	T0TCR = 0x03;
+	T0TCR = 0x00;
+}
+void LCD_init()
+{
+	IODIR0 = LCD_D|RS|E;
+	LCD_cmd(0x01);
+	LCD_cmd(0x02);
+	LCD_cmd(0x0C);
+	LCD_cmd(0x38);
+}
+
+void LCD_cmd(unsigned int cmd)
+{
+	IOCLR0 = LCD_D;
+	IOCLR0 = RS;
+	IOSET0 = cmd;
+	IOSET0 = E;
+	delayms(2);
+	IOCLR0 = E;
+}
+
+void LCD_data(unsigned char d)
+{
+	IOCLR0 = LCD_D;
+	IOSET0 = RS;
+	IOSET0 = d;
+	IOSET0 = E;
+	delayms(2);
+	IOCLR0 = E;
+}
+
+void LCD_str(unsigned char* s)
+{
+	while(*s)
+	{
+		LCD_data(*s++);
+		delayms(20);
+	}
+}
 

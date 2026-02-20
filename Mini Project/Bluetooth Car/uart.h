73aab9c3527d6b25e719e3276ff7c061d9092a5b
Srisuryaprakash <79654026+Sri-0104@users.noreply.github.com>
2026-02-20 12:23:40 +0530

    Update uart.h

--- Mini Project/Bluetooth Car/uart.h
+++ Mini Project/Bluetooth Car/uart.h
@@ -5,15 +5,15 @@ unsigned char UART_Rx(void);
 
 void UART_config(void)
 {
-	PINSEL0 = 0x04;
-	U0LCR = 0x83;
-	U0DLL = 97;
-	U0DLM = 0;
-	U0LCR = 0x03;
+	PINSEL0 = 0x50000;
+	U1LCR = 0x83;
+	U1DLL = 97;
+	U1DLM = 0;
+	U1LCR = 0x03;
 }
 
 unsigned char UART_Rx(void)
 {
-	while((U0LSR&0)==0);
-	return U0RBR;
+	while((U1LSR&1)==0);
+	return U1RBR;
 }
